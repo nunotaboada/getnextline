4aebb39a6879286c812106b608d7844c7577e86d
Nuno Filipe Moreira Taboada <nmoreira@c1r15s8.42porto.com>
2022-11-15 13:57:00 +0000

    entrega

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -3,151 +3,80 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmoreira <nmoreira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nmoreira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/09 12:14:26 by nmoreira          #+#    #+#             */
-/*   Updated: 2022/10/09 12:14:26 by nmoreira         ###   ########.fr       */
+/*   Created: 2022/11/14 14:00:16 by nmoreira          #+#    #+#             */
+/*   Updated: 2022/11/14 14:00:23 by nmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
- *	GET_NEXT_LINE
- *	-------------
- *	DESCRIPTION
- *	This function takes an opened file descriptor and returns its next line.
- *	This function has undefined behavior when reading from a binary file.
- *	PARAMETERS
- *	#1. A file descriptor
- *	RETURN VALUES
- *	If successful, get_next_line returns a string with the full line ending in
- *	a line break (`\n`) when there is one.
- *	If an error occurs, or there's nothing more to read, it returns NULL.
- *	----------------------------------------------------------------------------
- *	AUXILIARY FUNCTIONS
- *	-------------------
- *	READ_TO_LEFT_STR
- *	-----------------
- *	DESCRIPTION
- *	Takes the opened file descriptor and saves on a "buff" variable what readed
- *	from it. Then joins it to the cumulative static variable for the persistence
- *	of the information.
- *	PARAMETERS
- *	#1. A file descriptor.
- *	#2. The pointer to the cumulative static variable from previous runs of
- *	get_next_line.
- *	RETURN VALUES
- *	The new static variable value with buffer joined for the persistence of the info,
- *	or NULL if error.
- */
-
 #include "get_next_line_bonus.h"
-#include <stdio.h>
+/*#include <stdio.h>
 #include <fcntl.h>
-#include <limits.h>
+#include <limits.h>*/
 
-// retorno a linha lida se tiver /n
-static char *ft_read_line(int fd, char *buf, char *backup)
+static char	*ft_read_line(int fd, char *buf, char *backup)
 {
-    int bytesread;
-    char *tempbuf;
+	int		bytesread;
+	char	*tempbuf;
 
-    bytesread = 1;
-    while (bytesread != 0)
-    {
-        // read returns
-        // return Number of bytes read on success
-        // return 0 on reaching end of file
-        // return -1 on error
-        // return -1 on signal interrupt
-        bytesread = read(fd, buf, BUFFER_SIZE);
-        if (bytesread == -1)
-            return (0); // ou será NULL
-        if (bytesread == 0)
-            break;
-        buf[bytesread] = '\0'; // testar com printf se é bytesread ou bytesread -1
-        if (!backup)
-            backup = ft_strdup("");
-        tempbuf = backup;
-        backup = ft_strjoin(tempbuf, buf);
-        free(tempbuf);
-        tempbuf = NULL;
-        if (ft_strchr(buf, '\n'))
-            break;
-    }
-    return (backup);
+	bytesread = 1;
+	while (bytesread != 0)
+	{
+		bytesread = read(fd, buf, BUFFER_SIZE);
+		if (bytesread == -1)
+			return (0);
+		if (bytesread == 0)
+			break ;
+		buf[bytesread] = '\0';
+		if (!backup)
+			backup = ft_strdup("");
+		tempbuf = backup;
+		backup = ft_strjoin(tempbuf, buf);
+		free(tempbuf);
+		tempbuf = NULL;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	return (backup);
 }
 
-// vai retornar o backup
-
-static char *ft_get_line(char *line)
+static char	*ft_get_line(char *line)
 {
-    size_t count;
-    char *backup;
+	size_t	count;
+	char	*backup;
 
-    count = 0;
-    while (line[count] != '\n' && line[count])
-        count++;
-    if (line[count] == '\0')
-        return (0);
-    backup = ft_substr(line, count + 1, ft_strlen(line) - count); // count + 1 porque The returned line should include the ’\n’, só quando chega ao fim do ficheiro é que este não está incluído.
-    if (*backup == '\0') // este ciclo verifica se as linha acabaram
-    {
-        free(backup);
-        backup = NULL;
-    }
-    line[count + 1] = '\0';
-    return (backup);
+	count = 0;
+	while (line[count] != '\n' && line[count])
+		count++;
+	if (line[count] == '\0')
+		return (0);
+	backup = ft_substr(line, count + 1, ft_strlen(line) - count);
+	if (*backup == '\0')
+	{
+		free(backup);
+		backup = NULL;
+	}
+	line[count + 1] = '\0';
+	return (backup);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char *line;
-    char *buf;
-    static char *resto[FILES_OPEN];
+	char		*line;
+	char		*buf;
+	static char	*resto[1024];
 
-    if (fd < 0 || BUFFER_SIZE <= 0 || fd > FILES_OPEN)
-        return (NULL);
-    // buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    buf = malloc(sizeof *buf * (BUFFER_SIZE + 1));
-    if (!buf)
-        return (NULL);
-    line = ft_read_line(fd, buf, resto[fd]);
-    free(buf);
-    buf = NULL;
-    if (!line)
-        return (NULL);
-    resto[fd] = ft_get_line(line);
-    return (line);
-}
-
-int main(int argc, char **argv)
-{
-    char *line;
-    int i;
-    int fd1;
-    int fd2;
-    int fd3;
-
-    (void)argc;
-    fd1 = open(argv[1], O_RDONLY);
-    fd2 = open(argv[2], O_RDONLY);
-    fd3 = open(argv[3], O_RDONLY);
-    i = 1;
-    while (i < 7)
-    {
-        line = get_next_line(fd1);
-        printf("line [%02d]: %s", i, line);
-        free(line);
-        line = get_next_line(fd2);
-        printf("line [%02d]: %s", i, line);
-        free(line);
-        line = get_next_line(fd3);
-        printf("line [%02d]: %s", i, line);
-        free(line);
-        i++;
-    }
-    close(fd1);
-    close(fd2);
-    close(fd3);
-    return (0);
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 1024)
+		return (NULL);
+	buf = malloc(sizeof * buf * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	line = ft_read_line(fd, buf, resto[fd]);
+	free(buf);
+	buf = NULL;
+	if (!line)
+		return (NULL);
+	resto[fd] = ft_get_line(line);
+	return (line);
 }
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -3,150 +3,113 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmoreira <nmoreira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nmoreira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/09 12:17:32 by nmoreira          #+#    #+#             */
-/*   Updated: 2022/10/09 12:17:32 by nmoreira         ###   ########.fr       */
+/*   Created: 2022/11/14 14:16:41 by nmoreira          #+#    #+#             */
+/*   Updated: 2022/11/14 14:16:47 by nmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-size_t ft_strlen(const char *str)
+size_t	ft_strlen(const char *str)
 {
-    size_t i = 0;
-    while (*str)
-    {
-        str++;
-        i++;
-    }
-    return (i);
+	size_t	i;
+
+	i = 0;
+	while (*str)
+	{
+		str++;
+		i++;
+	}
+	return (i);
 }
 
-/*The strchr() function locates the first occurrence of c (converted to a
- *	char) in the string pointed to by s. The terminating null character is
- *	considered to be part of the string; therefore if c is `\0', the functions
- *	locate the terminating `\0'.
-*/
-char    *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    while (*s)
-    {
-        if (*s == c)
-            return ((char *)s);
-        s++;
-    }
-    if (c == '\0')
-        return ((char *)s);
-    return (NULL);
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
 }
 
-/*
-* outra solução para ft_strchr
-*
-* char	*ft_strchr(const char *str, int c)
-* {
-*	if (!str)
-*		return (NULL);
-*	while (*str)
-*	* and ++ operators have same precedence but right to left associativity. So * *   first ++ operator will be applied which is post-increment in this case therefore *   first a will be printed then str will point to character b
-*		if (*str++ == (char)c)
-*			return ((char *)(str - 1));
-*	if (*str == (char)c)
-*		return ((char *)str);
-*	return (NULL);
-*}
-*/
-
-
- /*	DESCRIPTION
- *	The strdup() function allocates sufficient memory for a copy of the string
- *	s1, does the copy, and returns a pointer to it.
-*/
-char *ft_strdup(const char *str)
+char	*ft_strdup(const char *str)
 {
-    int i = 0;
-    char *res;
+	int		i;
+	char	*res;
 
-    while (str[i])
-        i++;
-    res = (char *)malloc(sizeof(char) * (i + 1));
-    if (res == NULL)
-        return NULL;
-    i = 0;
-    while (str[i])
-    {
-        res[i] = str[i];
-        i++;
-    }
-    res[i] = '\0';
-    return (res);
+	i = 0;
+	while (str[i])
+		i++;
+	res = (char *)malloc(sizeof(char) * (i + 1));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		res[i] = str[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
 
-/*	DESCRIPTION
- *	Allocates (with malloc(3)) and returns a new string, which is the result of
- *	the concatenation of ’s1’ and ’s2’.
- *	PARAMETERS
-*/
-
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    size_t len = 0;
-    size_t i = 0;
-    size_t j = 0;
-    char *str;
+	size_t	i;
+	size_t	j;
+	char	*str;
 
-    if (!s1 || !s2)
-        return (NULL);
-    len = ft_strlen(s1) + ft_strlen(s2);
-    str = (char *)malloc(sizeof(char) * (len + 1));
-    if (!str)
-        return (NULL);
-    while (s1[i])
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    while (s2[j])
-    {
-        str[i + j] = s2[j];
-        j++;
-    }
-    str[i + j] = '\0';
-    return (str);
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
 }
-
-/*
-*	DESCRIPTION
-*	Allocates (with malloc()) and returns a substring from the string ’s’.
-*	The substring begins at index ’start’ and is of maximum size ’len’.
-*	PARAMETERS
-*/
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    unsigned int i; //usar os unsigned int está relacionado com os intervalos de representação
-    unsigned int j;
-    char *res;
+	unsigned int	i;
+	unsigned int	j;
+	char			*res;
 
-    i = 0;
-    j = 0;
-    if (!s)
-        return (NULL);
-    if (start >= ft_strlen(s))
-            return (ft_strdup("")); //temos que usar esta função porque temos que criar o resultado com malloc.
-    res = (char *)malloc(sizeof(char) * (len + 1));
-    if (!res)
-        return (NULL);
-    while (s[i])
-    {
-        if (i >= start && j < len )  // é < que len porque é uma string e começa em 0
-        {
-            res[j] = s[i];
-            j++;
-        }
-        i++;
-    }
-    res[j] = '\0';    
-    return (res);    
+	i = 0;
+	j = 0;
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			res[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	res[j] = '\0';
+	return (res);
 }
